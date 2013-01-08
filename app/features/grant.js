2a56045dea0e8664496e2167d9d1d420e36e089b
Robert Onodi <usdh_1989@yahoo.com>
2013-01-08 09:07:52 +0200

    updated grant feature

--- app/features/grant.js
+++ app/features/grant.js
@@ -1,6 +1,5 @@
 var underscore = require('underscore');
 
 module.exports = function(collaborators, user) {
-  console.log(collaborators, user, underscore.contains(collaborators, user));
   return underscore.contains(collaborators, user);
 };
