35638bdf7491351f34e160634a4c8f0f8d431636
Robert Onodi <robert.onodi@evozon.com>
2013-01-08 15:27:22 +0200

    updated dependencies, and removed console.log

--- app/features/grant.js
+++ app/features/grant.js
@@ -1,5 +1,12 @@
 var underscore = require('underscore');
 
+/**
+ * Check if the entity is part of a list
+ * 
+ * @param {Array} collaborators list
+ * @param {String|Number} user a user's id
+ */
+
 module.exports = function(collaborators, user) {
   return underscore.contains(collaborators, user);
 };
