b81956eb8b95c8f8caea86222411ef1397947f96
Robert Onodi <usdh_1989@yahoo.com>
2013-01-17 10:48:09 +0200

    rewrote tests

--- test/authentication_test.js
+++ test/authentication_test.js
@@ -1,92 +1,130 @@
-// /**
- // * Important! Set the environment to test
- // */
-// process.env.NODE_ENV = 'test';
-// 
-// var root = __dirname + '/../', 
-    // utils = require(root + 'lib/utils'),
-    // http = require('http'),
-    // resourceful = require('resourceful'),
-    // colors = require('colors'),
-    // chai = require('chai'), 
-    // should = chai.should(), 
-    // request = require('request'),
-    // api = '/api', 
-    // config, 
-    // db, 
-    // URL, 
-    // ENV;
-// 
-// config = utils.loadConfig();
-// ENV = process.env.NODE_ENV;
-// URL = utils.createBaseUrl(config['host'], config['port'], config['https']);
-// 
-// describe('Authentication::API'.yellow, function() {
-  // var User, 
-      // userObj = {
-        // email : 'john.doe@todoit.com',
-        // password : 'john!2345'
-      // };
-// 
-  // before(function(done) {
-    // db = resourceful.connection.connection;
-// 
-    // User = resourceful.resources.User;
-// 
-    // utils.cleanDb(db, done);
-  // });
-// 
-  // after(function(done) {
-    // utils.cleanDb(db, done);
-  // });
-// 
-  // describe('#User Login'.cyan, function() {
-//     
-    // before(function(done) {
-      // User.hashPassword(userObj.password, function(err, password, salt) {
-        // if (err) throw err;
-//         
-        // User.create({
-          // email: userObj.email,
-          // password: password,
-          // password_salt: salt
-        // }, function(err, user) {
-          // if (err) throw err;
-//           
-          // done();
-        // });
-      // });
-    // });
-//     
-    // after(function(done) {
-      // request({
-        // method : 'GET',
-        // url : URL + '/logout'
-      // }, function(err, res, body) {
-        // if (err) throw err;
-// 
-        // res.statusCode.should.equal(200);
-// 
-        // done();
-      // });      
-    // });
-//     
-    // it('should pass for valid credentials', function(done) {
-      // request({
-        // method : 'POST',
-        // url : URL + '/login',
-        // form : {
-          // username : userObj.email,
-          // password : userObj.password
-        // }
-      // }, function(err, res, body) {
-        // if (err) throw err;
-// 
-        // res.statusCode.should.equal(302);
-        // res.headers.location.should.equal('/dashboard');
-//         
-        // done();
-      // });
-    // });
-  // });
-// }); 
+/**
+ * Important! Set the environment to test
+ */
+process.env.NODE_ENV = 'test';
+
+var root = __dirname + '/../';
+var utils = require(root + 'lib/utils');
+var http = require('http');
+var colors = require('colors');
+var chai = require('chai');
+var should = chai.should();
+var request = require('request');
+var async = require('async');
+var api;
+var config;
+var db;
+var URL;
+var ENV = process.env.NODE_ENV;
+/**
+ * Require app
+ */
+var appServer;
+var app = require(root + 'app');
+
+config = utils.loadConfig();
+api = config['api_url'];
+URL = utils.createBaseUrl(config['host'], config['port'], config['https']);
+
+describe('Authentication::API'.yellow, function() {
+  var models, User, Project, Collaborator, projectId, userId, seconduserId;
+  var mockUser = {
+    username: ['test@todoit.com', 'bob@todoit.com'],
+    password: 'pass123'
+  };
+
+  before(function(done) {
+    models = app.get('models');
+    User = models.User;
+
+    appServer = http.createServer(app);
+        
+    appServer.on('listening', function() {
+      utils.cleanDb([User], function() {
+        async.auto({
+          hash_password: function(callback) {
+            User.hashPassword(mockUser.password, function(err, password, salt) {
+              callback(err, {
+                password: password,
+                salt: salt
+              });
+            });
+          },
+          create_users: ['hash_password', function(callback, result) {
+            User.create({
+              email: mockUser.username[0],
+              password: result.hash_password.password,
+              password_salt: result.hash_password.salt
+            }, function(err, user) {
+              if (err) throw err;
+  
+              userId = user.id
+              
+              User.create({
+                email: mockUser.username[1],
+                password: result.hash_password.password,
+                password_salt: result.hash_password.salt
+              }, function(err, anotherUser) {
+                seconduserId = anotherUser.id
+              
+                callback(err, {
+                  user_one: user,
+                  user_two: anotherUser
+                });
+              });
+            });
+          }]
+        }, function(err, results) {
+          if (err) throw err;
+          
+          done();
+        });
+      });
+    });
+    
+    appServer.listen(config.port);
+  });
+
+  after(function(done) {
+    appServer.on('close', function() {
+      setTimeout(done, 500);
+    });
+    utils.cleanDb([User], function() {
+      appServer.close();
+    });
+  });
+
+  describe('#User Login'.cyan, function() {
+    
+    after(function(done) {
+      request({
+        method : 'GET',
+        url : URL + '/logout'
+      }, function(err, res, body) {
+        if (err) throw err;
+
+        res.statusCode.should.equal(200);
+
+        done();
+      });
+    });
+    
+    it('should pass for valid credentials', function(done) {
+      request({
+        method : 'POST',
+        url : URL + '/login',
+        form : {
+          username : mockUser.username[0],
+          password : mockUser.password
+        }
+      }, function(err, res, body) {
+        if (err) throw err;
+
+        res.statusCode.should.equal(302);
+        res.headers.location.should.equal('/dashboard');
+        
+        done();
+      });
+    });
+  });
+});
--- test/models/user_test.js
+++ test/models/user_test.js
@@ -1,62 +1,60 @@
-// /**
- // * Set the environment to test
- // */
-// process.env.NODE_ENV = 'test';
-// 
-// var resourceful = require('resourceful'),
-    // root = __dirname + '/../../',
-    // utils = require(root + 'lib/utils'),
-    // colors = require('colors'),
-    // chai = require('chai'),
-    // expect = chai.expect,
-    // should = chai.should(),
-    // request = require('request'),
-    // config = utils.loadConfig(),
-    // api = '/api',
-    // //app = require(root + 'app'),
-    // db, URL, ENV;
-// 
-// ENV = process.env.NODE_ENV;
-// 
-// describe('User::Model'.yellow, function() {
-  // var User, values = {
-    // email : 'john.doe@todos.com',
-    // password : 'john!2345'
-  // };
-// 
-  // before(function(done) {
-    // resourceful.use(config.storage.engine, {
-      // database : config.storage.database
-    // });
-    // /**
-     // * Get the database object
-     // */
-    // db = resourceful.connection.connection;
-// 
-    // User = require(root + 'app/models/user_model')(resourceful);
-    // /**
-     // * Clean database
-     // */
-    // utils.cleanDb(db, done);
-// 
-  // });
-// 
-  // after(function(done) {
-    // utils.cleanDb(db, done);
-  // });
-// 
+/**
+ * Set the environment to test
+ */
+process.env.NODE_ENV = 'test';
+
+var root = __dirname + '/../../';
+var utils = require(root + 'lib/utils');
+var http = require('http');
+var colors = require('colors');
+var chai = require('chai');
+var should = chai.should();
+var request = require('request');
+var async = require('async');
+var api;
+var config;
+var db;
+var URL;
+var ENV = process.env.NODE_ENV;
+/**
+ * Require app
+ */
+var appServer;
+var app = require(root + 'app');
+
+config = utils.loadConfig();
+api = config['api_url'];
+URL = utils.createBaseUrl(config['host'], config['port'], config['https']);
+
+describe('User::Model'.yellow, function() {
+  var models, User, Project, Collaborator, projectId, userId, seconduserId;
+  var mockUser = {
+    username: ['test@todoit.com', 'bob@todoit.com'],
+    password: 'pass123'
+  };
+
+  before(function(done) {
+    models = app.get('models');
+    User = models.User;
+ 
+    appServer = http.createServer(app);
+    appServer.on('listening', function() {
+      utils.cleanDb([User], done);
+    });
+    
+    appServer.listen(config.port);
+  });
+
+  after(function(done) {
+    appServer.on('close', function() {
+      setTimeout(done, 500);
+    });
+    utils.cleanDb([User], function() {
+      appServer.close();
+    });
+  });
+
   // describe('#Create User'.cyan, function() {
-// 
-    // it('should have email and password required', function(done) {
-      // var newUser = User.create({}, function(err, result) {
-        // err.validate.errors.should.be.an('array');
-        // err.validate.errors.length.should.equal(2);
-        // err.validate.errors[0].property.should.equal('email');
-        // err.validate.errors[1].property.should.equal('password');
-        // done();
-      // });
-    // });
-// 
     // it('should not create for an invalid email', function(done) {
       // var newUser = User.create({
         // email : 'john.doe@todos',
@@ -66,78 +64,32 @@
         // done();
       // });
     // });
-// 
-    // it('should not create for password length less then 8', function(done) {
-      // var newUser = User.create({
-        // email : 'john.doe@todos.com',
-        // password : 'john!'
-      // }, function(err, result) {
-        // err.validate.errors[0].property.should.equal('password');
-        // done();
-      // });
-    // });
-// 
-    // // it('should encrypt password', function(done) {
-    // // var newUser = User.create({
-    // // email: values.email,
-    // // password: values.password
-    // // }, function(err, result) {
-    // // result.password.should.not.equal(values.password);
-    // // done();
-    // // });
-    // // })
   // });
-// 
-  // describe('#Get User'.cyan, function(done) {
-//     
-    // it('should return user without password and salt when calling with safeJSON', function(done) {
-      // User.hashPassword(values.password, function(err, password, salt) {
-        // if (err) throw err;
-//         
-        // User.create({
-          // 'email' : values.email,
-          // 'password' : password,
-          // 'password_salt' : salt
-        // }, function(err, user) {
-          // if (err)
-            // throw err;
-// 
-          // user.safeJSON().should.not.include.keys('password', 'password_salt');
-          // done();
-        // });
-      // });
-    // });
-  // });
-// 
-  // describe('#Check User credentials'.cyan, function() {
-    // var newValues = {
-      // email : 'jane.doe@evozon.com',
-      // password : 'jane!2345'
-    // };
-// 
-    // before(function(done) {
-      // User.hashPassword(newValues.password, function(err, password, salt) {
-        // if (err) throw err;
-//         
-        // User.create({
-          // 'email' : newValues.email,
-          // 'password' : password,
-          // 'password_salt' : salt
-        // }, function(err, user) {
-          // if (err)
-            // throw err;
-// 
-          // done();
-        // });
-      // });
-    // });
-// 
-    // it('should find the user for valid credentials', function(done) {
-      // User.checkCredentials(newValues.email, newValues.password, function(err, result) {
-        // result.should.not.equal(null);
-// 
-        // done();
-      // });
-    // });
-  // });
-// });
+
+  describe('#Check User credentials'.cyan, function() {
+    before(function(done) {
+      User.hashPassword(mockUser.password, function(err, password, salt) {
+        if (err) throw err;
+        
+        User.create({
+          'email' : mockUser.username[0],
+          'password' : password,
+          'password_salt' : salt
+        }, function(err, user) {
+          if (err)
+            throw err;
+
+          done();
+        });
+      });
+    });
+
+    it('should find the user for valid credentials', function(done) {
+      User.checkCredentials(mockUser.username[0], mockUser.password, function(err, result) {
+        result.should.not.equal(null);
+
+        done();
+      });
+    });
+  });
+});
